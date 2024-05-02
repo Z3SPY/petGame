dc532b6bf041e00fc661a2cf0695dcfb49442dae
joshua jabat <joshuajabat@gmail.com>
2024-05-02 19:36:10 +0800

    fixes

--- src/gameScript.ts
+++ src/gameScript.ts
@@ -4,13 +4,12 @@ const canvas = document.getElementById('gameCanvas') as HTMLCanvasElement;
 const ctx  = canvas?.getContext('2d')!;
 
 
-const CANVAS_WIDTH : number = canvas.width = 1000;
-const CANVAS_HEIGHT : number = canvas.height = 1000;
+const CANVAS_WIDTH : number =  canvas.width = canvas.getBoundingClientRect().width;
+const CANVAS_HEIGHT : number = canvas.height = canvas.getBoundingClientRect().height;
+
 
 
 ctx.fillStyle = 'white';
-
-
 
 
 const hungerText: HTMLSpanElement = document.getElementById("hngrValTxt")!.getElementsByTagName("span")[0];
@@ -48,7 +47,11 @@ class Animal {
     randY!: number;
     state: AnimalState = AnimalState.Moving; // Initial state is Idle
 
+    // Handle Animation 
+    imgNumber : number = 1;
+    lastImgNumber : number = 2;
 
+    
     constructor(xPos:number , yPos:number, w:number, h:number) {
         this.hungerVal = 100;
         this.cleanVal = 100
@@ -58,6 +61,8 @@ class Animal {
         this.yPos = yPos;
         this.w = w;
         this.h = h;
+
+        console.log(CANVAS_HEIGHT);
 
 
         this.randX = Math.floor(Math.random() * 3) - 1; // -1, 0, or 1
@@ -74,7 +79,6 @@ class Animal {
                 this.updateState(AnimalState.Moving);
             }, idleInterval);
         }, moveInterval);
-
        
         
     }
@@ -100,10 +104,10 @@ class Animal {
 
 
     handleBoundaries() {
-        if (this.xPos + this.w > CANVAS_WIDTH || this.xPos < 0) {
+        if (this.xPos + this.w >= CANVAS_WIDTH || this.xPos < 0) {
             this.randX *= -1; // Reverse direction on hitting horizontal boundaries
         }
-        if (this.yPos + this.h > CANVAS_HEIGHT || this.yPos < 0) {
+        if (this.yPos + this.h >= CANVAS_HEIGHT || this.yPos < 0) {
             this.randY *= -1; // Reverse direction on hitting vertical boundaries
         }
     }
@@ -136,6 +140,27 @@ class Animal {
     }
 
 
+   
+    
+
+    //Draw at 15 frames per second
+    animateAnimal() {
+        const img  = new Image;
+        img.onload = function(){
+            ctx.clearRect( 0, 0, ctx.canvas.width, ctx.canvas.height );
+            ctx.drawImage( img, 0, 0 );
+            };
+            var timer = setInterval( () =>{
+            if (this.imgNumber>this.lastImgNumber){
+                clearInterval( timer );
+            } else {
+                img.src = "images/left_hnd_"+( this.imgNumber++ )+".png";
+            }
+            }, 1000/15 ); 
+            
+    }
+
+
     draw(ctx: CanvasRenderingContext2D, deltaTime: number) {
         
 
@@ -144,14 +169,35 @@ class Animal {
         this.performActions(deltaTime);
         this.handleBoundaries();
 
-
+        //console.log(`x pos: ${this.xPos}  y pos: ${this.yPos} \n max X: ${CANVAS_WIDTH} max Y: ${CANVAS_HEIGHT}`);
+        var spriteSheetURL = '/cool.png';
+        console.log(spriteSheetURL);
+        // create a new image from the spritesheet
+        var image = new Image();
+        image.src = spriteSheetURL;
+        image.crossOrigin = "true";
+        // once the spritesheet loads,
+        // draw it on the canvas
+        image.onload = function() {
+            ctx.drawImage(
+                image,
+                0,
+                0,
+                image.width,
+                image.height,
+                0,
+                0,
+                canvas.width,
+                canvas.height
+            );
+        };
 
     }
 
 
 }
 
-let myAnimal: Animal = new Animal(Math.random() * (CANVAS_WIDTH - 200), Math.random() * (CANVAS_HEIGHT - 200), 100, 100);
+let myAnimal: Animal = new Animal(Math.random() * (CANVAS_WIDTH * .50), Math.random() * (CANVAS_HEIGHT * .50), CANVAS_WIDTH * .05, CANVAS_HEIGHT * .10);
 
 let lastFrameTime = performance.now(); // Move lastFrameTime to the global scope
 
@@ -169,7 +215,7 @@ function animate() {
     animationId = requestAnimationFrame(animate);
 }
 
-animate(); // Start the animation loop initially
+//animate(); // Start the animation loop initially
 
 // Handle tab visibility change
 function handleVisibilityChange() {
@@ -188,3 +234,6 @@ function handleVisibilityChange() {
 }
 
 document.addEventListener("visibilitychange", handleVisibilityChange, false);
+
+
+
