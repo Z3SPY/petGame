89f048974eda3bda4ca2c954438046f24c6202a4
joshua jabat <joshuajabat@gmail.com>
2024-05-03 19:30:45 +0800

    hunger bar visuals, and other visual updates

--- src/gameScript.ts
+++ src/gameScript.ts
@@ -16,6 +16,10 @@ const hungerText: HTMLSpanElement = document.getElementById("hngrValTxt")!.getEl
 const cleanText: HTMLSpanElement = document.getElementById("clnValTxt")!.getElementsByTagName("span")[0];
 const enjyText: HTMLSpanElement = document.getElementById("enjyValTxt")!.getElementsByTagName("span")[0];
 
+let enjyFill : HTMLElement = document.querySelector('#enjyLoad .fill') as HTMLElement;
+let cleanFill : HTMLElement = document.querySelector('#clnValTxt .fill') as HTMLElement;
+let hungerFill : HTMLElement = document.querySelector('#hngrValTxt .fill') as HTMLElement;
+
 
 //StateMachine
 enum AnimalState {
@@ -25,177 +29,156 @@ enum AnimalState {
 }
 
 
-
-//End StateMachine
-let intervalId: number; // Declare intervalId as a global variable
-
 class Animal {
     //Status Values 
     hungerVal!: number;
     cleanVal!: number;
     playVal!: number;
 
-
     xPos: number;
     yPos: number;
     w: number;
     h: number;
-    speed: number = 100;
+    speed: number = 200;
 
-    
-    randX!: number;
-    randY!: number;
-    state: AnimalState = AnimalState.Moving; // Initial state is Idle
+    vx: number;
+    vy: number;
 
-    // Handle Animation 
-    imgNumber : number = 1;
-    lastImgNumber : number = 2;
+    state: AnimalState = AnimalState.Moving; // Initial state is Moving
 
-    
-    constructor(xPos:number , yPos:number, w:number, h:number) {
+    image: HTMLImageElement = new Image();
+    spriteSheetURL: string = '/cool-removebg-preview.png';
+    cols: number = 4;
+    rows: number = 2;
+    spriteWidth: number = 1000;
+    spriteHeight: number = 250;
+    spriteWdOfst: number = this.spriteWidth / this.cols; // width in px / cols spacing = Value of SpriteWidth
+    spriteHtOfst: number = this.spriteHeight / this.rows; // height in px / rows spacing = Sprite height
+    frameX: number = 0;
+    frameY: number = 0;
+    gameFrame: number = 0;
+    staggerFrames: number = 2;
+
+    constructor(xPos: number, yPos: number, w: number, h: number) {
         this.hungerVal = 100;
-        this.cleanVal = 100
+        this.cleanVal = 100;
         this.playVal = 100;
 
+
+        //Position and Size
         this.xPos = xPos;
         this.yPos = yPos;
         this.w = w;
         this.h = h;
-
-        console.log(CANVAS_HEIGHT);
-
-
-        this.randX = Math.floor(Math.random() * 3) - 1; // -1, 0, or 1
-        this.randY = Math.floor(Math.random() * 3) - 1; // -1, 0, or 1
-        let moveInterval = 5000;
-        let idleInterval = 2000;
+        this.vx = (Math.random() - 0.5) * this.speed; // Random number between -1 and 1
+        this.vy = (Math.random() - 0.5) * this.speed;
 
         this.updateStatus();
+        let moveInterval = 5000;
+        let idleInterval = 2000;
         setInterval(() => {
             // Transition to the Idle state after the interval finishes
             this.updateState(AnimalState.Idle);
             setTimeout(() => {
-            
                 this.updateState(AnimalState.Moving);
             }, idleInterval);
         }, moveInterval);
-       
-        
+
+        //Image 
+        this.image.src = this.spriteSheetURL;
     }
 
     updateStatus() {
-        hungerText.innerText = this.hungerVal.toString();
-        cleanText.innerText = this.cleanVal.toString();
-        enjyText.innerText = this.playVal.toString();
+        hungerText.innerText = `${this.hungerVal.toString()}%`;
+        cleanText.innerText = `${this.cleanVal.toString()}%`;
+        enjyText.innerText = `${this.playVal.toString()}%`;
+
+        hungerFill.style.width =  `${this.hungerVal.toString()}%`;
+        cleanFill.style.width =  `${this.cleanVal.toString()}%`;
+        enjyFill.style.width =  `${this.playVal.toString()}%`;
 
     }
 
-    //Hunger 
     decreaseHunger() {
         // Decrease hunger by a certain amount
-        this.hungerVal -= 10; // You can adjust the amount as needed
+        this.hungerVal -= 1; // You can adjust the amount as needed
         if (this.hungerVal < 0) {
             this.hungerVal = 0; // Ensure hungerVal doesn't go below 0
         }
         this.updateStatus(); // Update the hunger text
     }
-    
-  
 
+    decreaseClean() {
+        // Decrease hunger by a certain amount
+        this.cleanVal -= 1; // You can adjust the amount as needed
+        if (this.cleanVal < 0) {
+            this.cleanVal = 0; // Ensure hungerVal doesn't go below 0
+        }
+        this.updateStatus(); // Update the hunger text
+    }
+
+    decreaseEnjoyment() {
+        // Decrease hunger by a certain amount
+        this.playVal -= 1; // You can adjust the amount as needed
+        if (this.playVal < 0) {
+            this.playVal = 0; // Ensure hungerVal doesn't go below 0
+        }
+        this.updateStatus(); // Update the hunger text
+    }
 
     handleBoundaries() {
-        if (this.xPos + this.w >= CANVAS_WIDTH || this.xPos < 0) {
-            this.randX *= -1; // Reverse direction on hitting horizontal boundaries
+        if (this.xPos + this.w >= CANVAS_WIDTH * .75 || this.xPos < CANVAS_WIDTH * .25) {
+            this.vx *= -1; // Reverse direction on hitting horizontal boundaries
         }
         if (this.yPos + this.h >= CANVAS_HEIGHT || this.yPos < 0) {
-            this.randY *= -1; // Reverse direction on hitting vertical boundaries
+            this.vy *= -1; // Reverse direction on hitting vertical boundaries
         }
+
     }
-    
+
     updateState(newState: AnimalState) {
-        
         this.state = newState;
-        console.log(this.state);
-
     }
-
-    
 
     performActions(deltaTime: number) {
         switch (this.state) {
             case AnimalState.Idle:
-                // Perform actions for Idle state
-                this.randX = Math.floor(Math.random() * 3) - 1; // -1, 0, or 1
-                this.randY = Math.floor(Math.random() * 3) - 1; // -1, 0, or 1
-
                 break;
             case AnimalState.Moving:
-                const deltaX = (this.speed * deltaTime) / 1000; // Convert speed to pixels per millisecond
-                this.xPos += this.randX * deltaX;
-                this.yPos += this.randY * deltaX;
-    
-
+                const deltaX = this.vx * (deltaTime / 1000);
+                const deltaY = this.vy * (deltaTime / 1000);
+                this.xPos += deltaX;
+                this.yPos += deltaY;
                 break;
         }
     }
 
-
-   
-    
-
-    //Draw at 15 frames per second
-    animateAnimal() {
-        const img  = new Image;
-        img.onload = function(){
-            ctx.clearRect( 0, 0, ctx.canvas.width, ctx.canvas.height );
-            ctx.drawImage( img, 0, 0 );
-            };
-            var timer = setInterval( () =>{
-            if (this.imgNumber>this.lastImgNumber){
-                clearInterval( timer );
-            } else {
-                img.src = "images/left_hnd_"+( this.imgNumber++ )+".png";
-            }
-            }, 1000/15 ); 
-            
-    }
-
-
     draw(ctx: CanvasRenderingContext2D, deltaTime: number) {
-        
-
-        ctx.fillRect(this.xPos, this.yPos, this.w, this.h);        
-
         this.performActions(deltaTime);
         this.handleBoundaries();
 
-        //console.log(`x pos: ${this.xPos}  y pos: ${this.yPos} \n max X: ${CANVAS_WIDTH} max Y: ${CANVAS_HEIGHT}`);
-        var spriteSheetURL = '/cool.png';
-        console.log(spriteSheetURL);
-        // create a new image from the spritesheet
-        var image = new Image();
-        image.src = spriteSheetURL;
-        image.crossOrigin = "true";
-        // once the spritesheet loads,
-        // draw it on the canvas
-        image.onload = function() {
-            ctx.drawImage(
-                image,
-                0,
-                0,
-                image.width,
-                image.height,
-                0,
-                0,
-                canvas.width,
-                canvas.height
-            );
-        };
-
+        // Draw animal on the canvas
+        this.gameFrame += deltaTime;
+        if (this.gameFrame >= this.staggerFrames * (1000 / 15)) { // 1000 ms / 15 frames per second
+            this.gameFrame = 0;
+            if (this.frameX < this.cols - 1) this.frameX++;
+            else this.frameX = 0;
+        }
+        ctx.drawImage(
+            this.image,
+            this.spriteWdOfst * this.frameX,
+            this.spriteHtOfst * this.frameY,
+            this.spriteWdOfst,
+            this.spriteHtOfst,
+            this.xPos,
+            this.yPos,
+            this.w + 50,
+            this.h + 10
+        );
     }
-
-
 }
+
+
 
 let myAnimal: Animal = new Animal(Math.random() * (CANVAS_WIDTH * .50), Math.random() * (CANVAS_HEIGHT * .50), CANVAS_WIDTH * .05, CANVAS_HEIGHT * .10);
 
@@ -213,25 +196,50 @@ function animate() {
     myAnimal.draw(ctx, deltaTime);
 
     animationId = requestAnimationFrame(animate);
+    
 }
 
-//animate(); // Start the animation loop initially
+animate(); // Start the animation loop initially
+
+
+//End StateMachine
+let intervalId: number; // Declare intervalId as a global variable
+intervalId = setInterval(() => {
+    myAnimal.decreaseHunger(); // Resume interval for decreasing hunger
+}, 2000); 
+intervalId = setInterval(() => {
+    myAnimal.decreaseEnjoyment();
+}, 5000);
+intervalId = setInterval(() => {
+    myAnimal.decreaseClean();
+}, 3000);
 
 // Handle tab visibility change
 function handleVisibilityChange() {
     if (document.hidden) {
         // Tab is not visible, so pause the animation and intervals
         cancelAnimationFrame(animationId);
-        clearInterval(intervalId); // Replace 'intervalId' with the ID of your setInterval function
+        clearInterval(intervalId);
     } else {
         // Tab is visible, so resume the animation and intervals
         lastFrameTime = performance.now(); // Reset lastFrameTime to avoid large deltaTime on resume
         animate(); // Start animation loop again
+        
+        // Resume interval for decreasing hunger
         intervalId = setInterval(() => {
-            myAnimal.decreaseHunger(); // Resume interval for decreasing hunger
-        }, 20000); // Adjust the interval time as needed
+            myAnimal.decreaseHunger();
+        }, 2000); // Adjust the interval time as needed
+        intervalId = setInterval(() => {
+            myAnimal.decreaseEnjoyment();
+        }, 5000);
+        intervalId = setInterval(() => {
+            myAnimal.decreaseClean();
+        }, 3000);
     }
 }
+
+document.addEventListener("visibilitychange", handleVisibilityChange, false);
+
 
 document.addEventListener("visibilitychange", handleVisibilityChange, false);
 
